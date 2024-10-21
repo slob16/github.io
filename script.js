93928cd9570b1e7a5e038769d0900377522c7a2c
slob16 <sian@wetalentedfew.com>
2024-10-21 20:07:39 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add scroll event to change nav background on scroll
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
